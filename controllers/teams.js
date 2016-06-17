7a23e436d398fc261c8fab6ce5da00d49089409d
Ivar Lee <ivarr5@gmail.com>
2016-06-17 07:31:24 -0700

    test

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,28 +1,47 @@
 var express = require('express');
-var fs = require('fs');
 var teamService = require('../models/teamService');
 var router = express.Router();
 
+// Get teams/
 router.get('/', function(req, res) {
   var teams = teamService.allTeams();
   res.render('teams/index', { teams: teams });
 });
 
+// Post teams/
 router.post('/', function(req, res) {
   teamService.addTeam(req.body);
-
   res.redirect('/teams');
 });
 
+// Get teams/new
 router.get('/new', function(req, res) {
   res.render('teams/new');
 });
 
+// Get teams/:name
 router.get('/:name', function(req, res) {
   // search for the team name in all the teams.
   var team = teamService.getTeam(req.params.name);
-
   res.render('teams/show', { team: team });
+});
+
+// Delete teams/:name
+router.delete('/:name', function(req, res) {
+  teamService.deleteTeam(req.params.name);
+  res.send({ msg: 'deleted'});
+});
+
+// Get teams/:name/edit
+router.get('/:name/edit', function(req, res) {
+  var teamToEdit = teamService.getTeam(req.params.name);
+  res.render('teams/edit.ejs', { team : teamToEdit });
+});
+
+// Put /teams/:name
+router.put('/:name', function(req, res) {
+  teamService.editTeam(req.params.name, req.body);
+  res.send({ msg: 'edited! '});
 });
 
 module.exports = router;
--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
+$('.delete-link').on('click', function(e) {
+  e.preventDefault();
+  var teamUrl = $(this).attr('href');
+  var elementToDelete = $(this);
+  console.log(teamUrl);
+  $.ajax({
+    method: 'DELETE',
+    url: teamUrl
+  }).done(function(data) {
+    // removes the parent element of the element that we're trying to delete
+    elementToDelete.parent().remove();
+    console.log('Deleted!');
+    console.log(data);
+  });
+  });
 
+$('.put-form').submit(function(e) {
+  e.preventDefault();
+  var teamUrl = $(this).attr('action');
+  console.log('over riding')
+  var teamData = $(this).serialize();
+  console.log(teamData);
+  $.ajax({
+    url: teamUrl,
+    method: 'PUT',
+    data: teamData
+  }).done(function(data) {
+      console.log(data.msg);
+      window.location = '/teams';
+  });
+ });
 });
